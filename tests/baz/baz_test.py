40bba5bc7dd4121a4cf0e5a3ff3a37a94700b3bc
Denis Paluca <denispaluca@gmail.com>
2023-06-15 17:01:08 +0200

    Fix buzz tests

--- tests/baz/baz_test.py
+++ tests/baz/baz_test.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from baz_contract import compile
 from baz_eval import evaluate
-from src.ContractFuzzer import TotalContractFuzzer
+from algofuzz.FuzzAppClient import FuzzAppClient 
+from algofuzz.ContractFuzzer import TotalContractFuzzer
 
 
 def main():
-    fuzzer = TotalContractFuzzer(*compile())
+    approval, clear, contract, schema = compile()
+    app_client = FuzzAppClient.from_compiled(approval, clear, contract, schema)
+    fuzzer = TotalContractFuzzer(app_client)
     fuzzer.start(evaluate, 10000)
 
 if __name__ == '__main__':
